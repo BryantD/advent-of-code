98e53bd98e672c513b481ad49cdf7c99cb55f186
BryantD <durrell@innocence.com>
2022-12-02 08:04:09 -0800

    Obsessively ran black over this for formatting

--- 2022/01/solve.py
+++ 2022/01/solve.py
@@ -4,7 +4,7 @@
 def part1(data):
     biggest_elf_calories = 0
     current_elf_calories = 0
-    
+
     for line in data:
         if line == "":
             if current_elf_calories > biggest_elf_calories:
@@ -12,13 +12,14 @@ def part1(data):
             current_elf_calories = 0
         else:
             current_elf_calories += int(line)
-            
+
     return biggest_elf_calories
-    
+
+
 def part2(data):
     current_elf_calories = 0
     biggest_three_elf_calories = [0, 0, 0]
-    
+
     for line in data:
         if line == "":
             if current_elf_calories > biggest_three_elf_calories[0]:
@@ -28,8 +29,9 @@ def part2(data):
             current_elf_calories = 0
         else:
             current_elf_calories += int(line)
-    
+
     return sum(biggest_three_elf_calories)
+
 
 def main():
     with open("data.txt") as data_file:
@@ -37,7 +39,7 @@ def main():
     data_lines = data_string.splitlines()
     solution1 = part1(data_lines)
     print(solution1)
-    
+
     print()
 
     solution2 = part2(data_lines)
