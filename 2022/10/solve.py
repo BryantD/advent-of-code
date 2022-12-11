9f0fb1651ce7c907f44375e63446625bf26aa1a0
BryantD <durrell@innocence.com>
2022-12-10 20:57:12 -0800

    Text formatter for day 10, template for day 11

--- 2022/10/solve.py
+++ 2022/10/solve.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+
 def compute_registers(data):
     cycle = []
     register = 1
-    cycle.append((0, 0))         # Simplify for later, we always ignore this
+    cycle.append((0, 0))  # Simplify for later, we always ignore this
     cycle.append((len(cycle), register))
-    
+
     for line in data:
         if line[0:4] == "addx":
             cycle.append((len(cycle), register))
@@ -26,9 +27,10 @@ def draw_line(cycles):
             line += "#"
         else:
             line += "."
-            
+
     return line
-    
+
+
 def part1(data):
     cycle = compute_registers(data)
     return sum(map(lambda x: x[0] * x[1], cycle[20::40]))
@@ -38,9 +40,10 @@ def part2(data):
     output = ""
     cycle = compute_registers(data)
     for i in (1, 41, 81, 121, 161, 201):
-        output += draw_line(cycle[i:i+40]) + "\n"
+        output += draw_line(cycle[i : i + 40]) + "\n"
 
     return output
+
 
 def main():
     with open("data.txt") as data_file:
